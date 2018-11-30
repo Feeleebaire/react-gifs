a181ff71d5aa7bb2eb8f5b55fb6417feb494cf1f
Philibert Latournerie <philibert.latournerie@hotmail.fr>
2018-11-30 16:43:24 +0100

    finito

---
+++ src/components/app.jsx
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import GifsList from './gifs-list.jsx';
+import Search from './search.jsx';
+import Gif from './gif.jsx';
+import giphy from 'giphy-api';
+
+class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "xT9IgDEI1iZyb2wqo8"
+    }
+  }
+
+  search = (query) => {
+    giphy ("K0OEARN25AQCKlv3ov5POW2yuk5zWnro").search({
+        q: query,
+    },
+    (err, res) => {
+      this.setState({
+        gifs: res.data
+      });
+    });
+  }
+
+  selectGif = (id) => {
+    this.setState({
+      selectedGifId: id
+    });
+  }
+
+  render(){
+    return(
+    <div>
+      <div className="left-scene">
+        <Search searchfunction={this.search}/>
+        <div className="selected-gif">
+          <Gif id={this.state.selectedGifId}/>
+        </div>
+      </div>
+      <div className="right-scene">
+        <GifsList gifs= {this.state.gifs} selectGif={this.selectGif}/>
+      </div>
+    </div>
+  )};
+}
+
+export default App;
